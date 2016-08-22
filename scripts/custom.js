0b2b4033247d29cd8b903c0d84647b459dbc1148
Oversec <admin@oversec.io>
2016-08-22 08:03:27 +0200

    disabled preloader

--- scripts/custom.js
+++ scripts/custom.js
@@ -8,8 +8,8 @@ $(window).load(function() { // makes sure the whole site is loaded
 //------------------------------------------------------------------------
 //						PRELOADER SCRIPT
 //------------------------------------------------------------------------   
-    $('#preloader').delay(400).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('#preloader .inner').fadeOut(); // will first fade out the loading animation
+ //   $('#preloader').delay(400).fadeOut('slow'); // will fade out the white DIV that covers the website.
+ //   $('#preloader .inner').fadeOut(); // will first fade out the loading animation
 
 
 //------------------------------------------------------------------------
